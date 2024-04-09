20e83d1fc7f78e81f22ab506ba1ef5ed16f14328
Maria <lopatina.m.r@gmail.come>
2024-04-09 18:03:20 +0300

    remove whitespace

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int a = Integer.parseInt(System.console().readLine());
+        int b = Integer.parseInt(System.console().readLine());
+        int sum;
+        int difference;
+        int product;
+        int quotient;
+        if (a > b) {
+            System.out.println(String.format(" %d > %d", a, b));
+        } else if (a == b) {
+
+            System.out.println(String.format(" %d = %d", a, b));
         }
+        else {
+
+            System.out.println(String.format(" %d < %d", a, b));
+
+        }
+
+        sum = a + b;
+        System.out.println( a + " + " + b + " = " + sum );
+
+        difference = a - b;
+        System.out.println( a + " - " + b + " = " + difference );
+
+        product = a * b;
+        System.out.println( a + " * " + b + " = " + product );
+
+        quotient = a / b;
+        System.out.println( a + " / " + b + " = " + quotient );
     }
 }
